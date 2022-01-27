b45b3f1f97ec22a56c77d0b40dbd85d0712f2ea2
haroonatcha <haroonatcha@yahoo.com>
2022-01-26 21:42:50 -0600

    large institution set
    uncomment the second chunk of code in institution_set and cache the set identifying large institutions

--- munge/04_institution_set.R
+++ munge/04_institution_set.R
@@ -6,21 +6,21 @@ institutions <- c('College of DuPage', 'Waubonsee Community College',
 
 
 #this second group is colleges with more than 1k
-#mean incoming cohort size. Comment out to switch to this one
 
 #first aggregate to total yearly cohort size
-# temp <- aggregate(data$cohort_size,
-#                   by = list(data$institution,
-#                             data$year),
-#                   FUN = sum,
-#                   na.rm = TRUE)
-# 
-# colnames(temp) <- c('institution', 'year', 'cohort_size')
-# 
-# temp <- aggregate(temp$cohort_size,
-#                   by = list(temp$institution),
-#                   FUN = mean)
-# 
-# institutions <- temp$Group.1[temp$x >= 500]
+temp <- aggregate(data$cohort_size,
+                  by = list(data$institution,
+                            data$year),
+                  FUN = sum,
+                  na.rm = TRUE)
+
+colnames(temp) <- c('institution', 'year', 'cohort_size')
+
+temp <- aggregate(temp$cohort_size,
+                  by = list(temp$institution),
+                  FUN = mean)
+
+large_institutions <- temp$Group.1[temp$x >= 500]
 
 cache('institutions')
+cache('large_institutions')
